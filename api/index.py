c2c3930bc5abd843369f49532673c485e9788f80
lihaijun <player4sir@gmail.com>
2025-01-25 03:09:06 +0800

    fix requirements

--- api/index.py
+++ api/index.py
@@ -7,5 +7,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from main import app
 
 # Vercel 需要的处理函数
-def handler(request, context):
-    return app(request) 
+def handler(request):
+    return app
+
+# 为了本地测试
+if __name__ == "__main__":
+    app.run() 
